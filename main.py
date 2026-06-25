4a666882d0d095ef43f18542dffd279014bc1967
예령 김 <newlap1235@outlook.kr>
2026-06-25 20:03:37 +0900

    ㅇㅇ

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from units import Player
 
 print("=== Text RPG 게임 ===")
+print("이름을 입력해주세요.")
+player_name = input()
 player = Player()
+player.name = player_name
+
 while True:
     menu = int(input("1. 사냥터 가기 2. 캐릭터 정보 3. 종료 : "))
     match(menu):
---
+++ units.py
@@ -0,0 +1,8 @@
+class Player:
+    name="Player1"
+    hp = 100
+    atk = 10
+    speed = 10
+    defense = 10
+    def show_info(self):
+        print(self.name + " ")
